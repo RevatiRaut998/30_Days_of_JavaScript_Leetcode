a22e8497507ae925051fc8e3e0f7a93c3a5cf41b
Revati Raut <revatiraut9@gmail.com>
2025-09-14 18:55:18 +0530

    Add implementation for Timeout Cancellation

---
+++ Promises and Time/2715.js
@@ -0,0 +1,56 @@
+// 30 days of Javacsript : 2715. Timeout Cancellation
+/** Given a function fn, an array of arguments args, and a timeout t in milliseconds, 
+ * return a cancel function cancelFn.
+
+After a delay of cancelTimeMs, the returned cancel function cancelFn will be invoked.
+
+setTimeout(cancelFn, cancelTimeMs)
+Initially, the execution of the function fn should be delayed by t milliseconds.
+
+If, before the delay of t milliseconds, the function cancelFn is invoked, it should cancel 
+the delayed execution of fn. 
+Otherwise, if cancelFn is not invoked within the specified delay t, 
+fn should be executed with the provided args as arguments.
+
+*/
+
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+    // Schedule fn to run after t milliseconds
+    var timer = setTimeout(function() {
+        fn(...args); // spread the array into separate arguments
+    }, t);
+
+    // Return a cancel function
+    return function cancelFn() {
+        clearTimeout(timer); // cancels fn if not executed yet
+    };
+};
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 5;
+ *  const args = [2], t = 20, cancelTimeMs = 50;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  const maxT = Math.max(t, cancelTimeMs);
+ *           
+ *  setTimeout(cancel, cancelTimeMs);
+ *
+ *  setTimeout(() => {
+ *      console.log(result); // [{"time":20,"returned":10}]
+ *  }, maxT + 15)
+ */
