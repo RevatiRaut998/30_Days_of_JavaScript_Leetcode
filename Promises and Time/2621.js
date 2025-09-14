e13ad26d2ba2ed3a94c59bebcf4a05f1321e5161
Revati Raut <revatiraut9@gmail.com>
2025-09-14 18:19:59 +0530

    Add implementation of sleep function using promises

---
+++ Promises and Time/2621.js
@@ -0,0 +1,22 @@
+// 30 days of Javacsript : 2621. Sleep
+// Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. 
+// It can resolve any value.
+// Note that minor deviation from millis in the actual sleep duration is acceptable.
+
+ 
+
+async function sleep(millis) {
+    const t = new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            resolve();
+        }, millis);
+    });
+    
+    return t;
+}
+/*---------------------------------------------------------------------------------------------------------------*/
+
+//Cleaner way to write it :
+async function sleep(millis) {
+    return new Promise(resolve => setTimeout(resolve, millis));
+}
