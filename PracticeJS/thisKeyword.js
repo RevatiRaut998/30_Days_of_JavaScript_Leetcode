d0b32fb4d66d89144f2db93640dd4c0762338fc8
Revati Raut <revatiraut9@gmail.com>
2025-09-16 10:20:21 +0530

    Updated thisKeyword.js for object

--- PracticeJS/thisKeyword.js
+++ PracticeJS/thisKeyword.js
@@ -1,39 +1,67 @@
-//THIS KEYWORD
+// THIS KEYWORD
 
+// In global space
+console.log(this); 
+// -> In browsers: 'window' (global object)
+// -> In Node.js: 'global'
 
-// this in global space
-
-console.log(this);
-
-//In console you will see  - Window object | Window {window: Window, self: Window, document: document, name: '', location: Location, …}
-//this keyword in global space represents global object
-// In case of browser the global object is Window hence why you see window object
-// in other places it will be global object - it can be window, global or nayhting else depending on where youre running javascript
-
-function x(){
-  console.log(this); // the value depends on strict/non strict mode (undefined/window)
+// Inside a function
+function x() {
+  console.log(this);
 }
 x();
+// -> Non-strict mode: 'this' is substituted with global object (window in browsers)
+// -> Strict mode: 'this' is undefined
 
-// Inside a function when you use this in non strict mode - it prints another window object in console but its diff than the global one
-// this behaves differently in strict n non strict code
-// so when we have 'use strict' added at top of the js file and we use console.log(this) inside a function it will print UNDEFINED in console
+// NOTE: "this substitution" happens only in non-strict mode.
+// If 'this' would otherwise be undefined, JS substitutes it with the global object.
+
+// Function call examples:
+x();          // -> window (non-strict) | undefined (strict)
+window.x();   // -> window (always in browsers)
 
 
-// NOTE : this substitution - if value of this keyword is undefined or null , this will be replaced with global object 
-// only in non strict mode this substitution happens
+/*********************************************************************************************************************************************************************************************/
 
-//Question - What is the value of this keyword in a function
-//In Interviews say this - Value of this in function is undefined but beacuse JS has this substitution - so the value becomes equal to global object if strict mode is not used
+/* SUMMARY:
 
-// Value of this keyword also depends on how the function is called
+1. In the global scope:
+   - Browser: `this` === window
+   - Node.js: `this` === global
 
-x();// => value will be window object in non strict mode
-// in strict mode it will be undefined
+2. Inside a normal function:
+   - Strict mode:
+       x();        // this === undefined
+       window.x(); // this === window        VALUE OF THIS DEPENDS ON HOW THE FUNCTION IS CALLED
+   - Non-strict mode:
+       x();        // this === window (this substitution)
+       window.x(); // this === window
 
-//If yu execute function like :
-window.x() //=> value will be window object
+3. "This substitution":
+   - In non-strict mode, if `this` would be undefined, it is automatically replaced with the global object.
+   - This does not happen in strict mode.
+*/
 
-//If the function is called without any reference of an object then the value is undefined (Example - x();)
-// if its called with reference (Ex: window.x()), value will be window object
+/***********************************************************************************************************************************************************************************************/
 
+// THIS inside an object's method
+
+// If a function is defined inside an object, it’s called a METHOD.
+// In that case, `this` refers to the object on which the method is called.
+
+const obj = {
+  a: 10,
+  x: function() {
+    console.log(this);   // -> { a: 10, x: f }
+    console.log(this.a); // -> 10
+  }
+};
+
+// How the function is called determines `this`:
+obj.x(); 
+// -> `this` === obj
+
+/* CONCLUSION:
+Inside an object’s method, this refers to the object the method belongs to.
+The value of this is always based on how the function is called, not where it is defined.
+*/
