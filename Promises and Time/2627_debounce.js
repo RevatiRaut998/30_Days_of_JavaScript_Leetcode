90bf5f893dfd1e54bde64174c3c9ff97672d0239
Revati Raut <revatiraut9@gmail.com>
2025-09-20 21:18:21 +0530

    Updated Debounce in 30 Days of JS

--- Promises and Time/2627_debounce.js
+++ Promises and Time/2627_debounce.js
@@ -14,25 +14,42 @@ and the 3rd would be executed at 135ms.
 
 // To study Debounce function in JS - https://www.geeksforgeeks.org/javascript/debouncing-in-javascript/
 
+/**************************************************************************************************************/
+
+//Solution 1 
 var debounce = function (fn, t) {
-  let timeout;                                  // This variable will store the timer ID
-  return function (...args) {                  // Returns a new function that will be called
-    clearTimeout(timeout);                     // Clear any existing timer
+  let timeout; // This variable will store the timer ID
+  return function (...args) {
+    // Returns a new function that will be called
+    clearTimeout(timeout); // Clear any existing timer
     timeout = setTimeout(() => {
-      fn.apply(this, args);                    // Execute the original function after the delay
+      fn.apply(this, args); // Execute the original function after the delay
     }, t);
   };
 };
 
+//Solution 2 - No change , just Single-line arrow function
+function debounce(fn, t) {
+  let timerId;
+  return function (...args) {
+    clearTimeout(timerId); // cancel any existing timer
+    timerId = setTimeout(() => fn(...args), t); // set a function to go off when a time is complete
+  };
+}
+
+/**************************************************************************************************************/
+
+//LEARNINGS :
 // Function to be debounced
 function search(query) {
   console.log("Searching for:", query);
 }
 
+//Example:
 // Create a debounced version of the search function
 const dSearch = debounce(search, 100);
 
 // Simulate typing with multiple calls to the debounced function
 dSearch("Hello");
 dSearch("Hello, ");
-dSearch('Hello, World!');  // Only this call will trigger after 100ms
+dSearch("Hello, World!"); // Only this call will trigger after 100ms
